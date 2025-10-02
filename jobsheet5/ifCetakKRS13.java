6c29f524dc4c2043aeebde30c682ba350922e577
sixzenith-marvel <marvel@sixzenith.com>
2025-10-02 16:55:39 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS13.java
+++ jobsheet5/ifCetakKRS13.java
@@ -13,6 +13,8 @@ public class ifCetakKRS13 {
         if (uktLunas){
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else{
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
         }
     }
 }
