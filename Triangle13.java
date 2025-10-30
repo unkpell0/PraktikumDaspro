acabe435c607093427d57c570b9d2cc10cac2bed
sixzenith-marvel <marvel@sixzenith.com>
2025-10-30 17:42:57 +0700

    Modifikasi Percobaan 3

--- Triangle13.java
+++ Triangle13.java
@@ -6,12 +6,13 @@ public class Triangle13 {
         System.out.print("Masukkan nilai n = ");
         int n = sc.nextInt();
         int i = 0;
-        while(i <= n){
+        while(i < n){
             int j = 0;
-            while(j < i){
+            while(j <= i){
                 System.out.print("*");
                 j++;
             }
+            System.out.println();
             i++;
         }
     }
