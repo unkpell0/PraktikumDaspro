797ff39ea96671bb33567539698fb3f5b646f568
sixzenith-marvel <marvel@sixzenith.com>
2025-10-30 18:12:50 +0700

    Modifikasi Percobaan 4

--- jobsheet8/NilaiKelompok13.java
+++ jobsheet8/NilaiKelompok13.java
@@ -5,12 +5,16 @@ public class NilaiKelompok13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i, j, nilai;
-        float totalNilai, rataNilai;
+        double totalNilai, rataNilai;
+        double ratatertinggi = 0.0;
+        int keltertinggi = 0;
 
         for (i=1; i <= 6; i++){
             System.out.println("Kelompok " + i);
             totalNilai = 0;
+
             j = 1;
+
             while (j <= 5) { 
                 System.out.print("  Nilai dari kelompok penilai "+ j + ": ");
                 nilai = sc.nextInt();
@@ -19,7 +23,15 @@ public class NilaiKelompok13 {
             }
             rataNilai = totalNilai / 5;
 
-            System.out.println("Rata-rata nilai: "+ rataNilai);
+            System.out.println("Rata-rata nilai: "+ rataNilai +"\n");
+
+            if(ratatertinggi < rataNilai){
+                ratatertinggi = rataNilai;
+                keltertinggi = i;
+            }
         }
+
+        System.out.println("Kelompok dengan rata-rata tertinggi adalah kelompok: "+ keltertinggi);
+        System.out.println("dengan rata rata: " + ratatertinggi);
     }
 }
