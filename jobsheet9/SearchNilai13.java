84510e9d4c2fa9a6e51f21637ac7dd0ed81c4805
sixzenith-marvel <marvel@sixzenith.com>
2025-11-13 15:35:02 +0700

    Modifikasi Percobaan 4

--- jobsheet9/SearchNilai13.java
+++ jobsheet9/SearchNilai13.java
@@ -1,8 +1,26 @@
+
+import java.util.Scanner;
+
 public class SearchNilai13 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+        Scanner sc = new Scanner(System.in);
+
+        int nilaimhs = 0;
+        int key = 0;
         int hasil = 0;
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int nilai = sc.nextInt();
+
+        int[] arrNilai = new int[nilai];
+        for (int i = 0; i < nilai; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+": ");
+            nilaimhs = sc.nextInt();
+            arrNilai[i] = nilaimhs;
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        key = sc.nextInt();
 
         for (int i = 0; i < arrNilai.length; i++){
             if (key == arrNilai[i]){
@@ -10,8 +28,9 @@ public class SearchNilai13 {
                 break;
             }
         }
+
         System.out.println();
-        System.out.println("Nilai "+ key+" ketemu di indeks ke-"+hasil);
+        System.out.println("Nilai "+ key+" ketemu, merupakan nilai mahasiswa ke-"+ (hasil+1));
         System.out.println();
     }
 }
