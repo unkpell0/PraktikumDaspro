661288701dc08ea425cc4c9fe9a76ff24df68700
sixzenith-marvel <marvel@sixzenith.com>
2025-11-13 12:55:05 +0700

    Modifikasi Percobaan 3

--- jobsheet9/ArrayRataNilai13.java
+++ jobsheet9/ArrayRataNilai13.java
@@ -5,20 +5,42 @@ public class ArrayRataNilai13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        double totallulus = 0;
+        double totaltidak = 0;
+        int oranglulus = 0; 
+        int orangtidaklulus = 0;
+        double ratalulus = 0.0; 
+        double ratatidak = 0.0;
+        int lulus = 0;
 
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int mhs = sc.nextInt();
+        int[] nilaiMhs = new int[mhs];
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+ (i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70){
+                totallulus += nilaiMhs[i];
+                oranglulus++;
+            } else{
+                totaltidak += nilaiMhs[i];
+                orangtidaklulus++;
+            }
         }
 
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+ rata2);
+        for (int i = 0; i < nilaiMhs.length; i++){
+            if(nilaiMhs[i] > 70){
+                lulus++;
+            } 
+        }
+
+        ratalulus = totallulus/oranglulus;
+        ratatidak = totaltidak/orangtidaklulus;
+
+        System.out.println("Rata-rata nilai lulus = "+ ratalulus);
+        System.out.println("Rata-rata nilai tidak lulus = "+ ratatidak);
     }
 }
