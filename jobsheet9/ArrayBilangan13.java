a89eac6aaacf0932cd6eaed8f8728af3a9e187fe
sixzenith-marvel <marvel@sixzenith.com>
2025-11-13 11:20:47 +0700

    Modifikasi Percobaan 1

--- jobsheet9/ArrayBilangan13.java
+++ jobsheet9/ArrayBilangan13.java
@@ -1,13 +1,8 @@
 public class ArrayBilangan13 {
     public static void main(String[] args) {
-        int[] bil = new int[4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        int[] bil = {5, 12867, 7, 20000};
+        for (int i = 0; i <= 4; i++){
+            System.out.println(bil[i]);
+        }
     }
 }
