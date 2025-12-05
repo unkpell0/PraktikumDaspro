efdd2847557ddd4bbae70482e79aa7575694e9b7
sixzenith-marvel <marvel@sixzenith.com>
2025-12-05 15:26:11 +0700

    Modifikasi Percobaan 1

--- jobsheet11/Kafe13.java
+++ jobsheet11/Kafe13.java
@@ -1,14 +1,5 @@
 public class Kafe13 {
     public static void main(String[] args) {
-        Menu("Andi", true);
-    }
-
-    public static void Menu(String namaPelanggan, boolean isMember) {
-        System.out.println("Selamat datang, " + namaPelanggan + "!");
-
-        if (isMember){
-            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
-        }
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15.000");
         System.out.println("2. Cappucino - Rp 20.000");
