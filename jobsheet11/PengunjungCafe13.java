74ac47ece6eb968ce2daac2fb06f82c79b50ab61
sixzenith-marvel <marvel@sixzenith.com>
2025-12-07 18:36:34 +0700

    Modifikasi Percobaan 4

--- jobsheet11/PengunjungCafe13.java
+++ jobsheet11/PengunjungCafe13.java
@@ -7,8 +7,9 @@ public class PengunjungCafe13 {
 
     static void daftarPengunjung (String...namaPengunjung){
         System.out.println("Daftar Nama Pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++){
-            System.out.println("- " + namaPengunjung[i]);
+
+        for (String nama : namaPengunjung){
+            System.out.println("- " + nama);
         }
     }
 }
