7cd8521ff933d447df648498451e0d1b04df2177
sixzenith-marvel <marvel@sixzenith.com>
2025-12-07 16:42:13 +0700

    Percobaan 3

--- jobsheet11/Kafe13.java
+++ jobsheet11/Kafe13.java
@@ -1,6 +1,20 @@
+
+import java.util.Scanner;
+
 public class Kafe13 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Menu("Andi", true, "DISKON50");
+
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+
+        System.out.print("\nMasukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
     }
 
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
@@ -29,5 +43,12 @@ public class Kafe13 {
         System.out.println("6. Mie Goreng - Rp 18.000");
         System.out.println("=============================");
         System.out.println("Silahkan pilih menu yang Anda inginkan");
+    }
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem){
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
     }
 }
