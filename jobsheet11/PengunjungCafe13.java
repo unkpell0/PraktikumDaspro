52ce50332a289ee0b72063b1697d2fc1a6af76f7
sixzenith-marvel <marvel@sixzenith.com>
2025-12-07 18:19:42 +0700

    Percobaan 4

---
+++ jobsheet11/PengunjungCafe13.java
@@ -0,0 +1,14 @@
+public class PengunjungCafe13 {
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+
+    static void daftarPengunjung (String...namaPengunjung){
+        System.out.println("Daftar Nama Pengunjung:");
+        for (int i = 0; i < namaPengunjung.length; i++){
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+}
