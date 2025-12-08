f016fc2c174213adc94f66dbfd141defc2c73c49
sixzenith-marvel <marvel@sixzenith.com>
2025-12-08 13:19:14 +0700

    Modifikasi Percobaan 2

--- jobsheet12/Percobaan2.java
+++ jobsheet12/Percobaan2.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 public class Percobaan2 {
     static int hitungPangkat(int x, int y){
         if (y == 0){
+            System.out.print("1");
             return (1);
         } else {
+            System.out.print(x + " x ");
             return (x * hitungPangkat(x, y-1));
         }
     }
@@ -18,6 +20,10 @@ public class Percobaan2 {
         bilangan = sc.nextInt();
         System.out.print("Pangkat: ");
         pangkat = sc.nextInt();
-        System.out.println(hitungPangkat(bilangan, pangkat));
+
+        System.out.print("Deret: ");
+        int hasil = hitungPangkat(bilangan, pangkat);
+
+        System.out.println(" = " + hasil);
     }
 }
